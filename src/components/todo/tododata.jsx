dbedf89a6243eb5359324d7bbf49a164840f4c67
truongsteve <dotruong1994@gmail.com>
2024-08-26 16:27:28 +0700

    video 33

---
+++ src/components/todo/tododata.jsx
@@ -0,0 +1,11 @@
+
+const TodoData = () => {
+    return (
+        <div className='todo-data'>
+            <div>Learning React</div>
+            <div>Watch youtube</div>
+        </div>
+    );
+}
+
+export default TodoData;
