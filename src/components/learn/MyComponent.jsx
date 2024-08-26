65a0c5a3886439846225fc7259fb185fbc9ca993
truongsteve <dotruong1994@gmail.com>
2024-08-26 13:49:39 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,17 @@
 import './style.css';
 
 const MyFunction = () => {
+    // const steve = "Steve";
+    // const steve = 32;
+    // const steve = [1, 2, 3];
+    const steve = {
+        name: "STEVE",
+        age: "30"
+    }
     return (
         <>
-            <div>Steve and his first component update</div>
+            <div>{JSON.stringify(steve)} and his first component update</div>
+            <div>{console.log("Steve check js")}</div>
             <div className="child" style={{ borderRadius: "10px" }}>Child</div>
         </>
     );
